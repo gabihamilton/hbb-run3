de5df2a7f379e68ac00945b0a3b441b02ed457ef
Gabi Hamilton <gabihamilton@gmail.com>
2026-01-30 11:48:44 -0600

    cleaning up

--- fitting/run_cards_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG="25Nov11_v14_private"
-
-for YEAR in 2022 2022EE 2023 2023BPix
-do
-    echo "Making Card for $YEAR..."
-    python3 make_zg_cards.py --year $YEAR --tag $TAG
-done
