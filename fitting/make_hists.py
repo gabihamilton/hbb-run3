80c3ccaa0908c5f61ae02a01952eb9055a8542f7
Gabi Hamilton <gabihamilton@gmail.com>
2026-04-08 11:30:27 -0500

    path to gabis skims

--- fitting/make_hists.py
+++ fitting/make_hists.py
@@ -152,7 +152,7 @@ def export_to_root(histograms, output_root_path, region_key, samples_qq, syst, d
                     if should_split:
                         fout[f"{base}bb_{suffix}"] = h[:, i_bin, category, 3]
                         fout[f"{base}c_{suffix}"] = h[:, i_bin, category, 2]
-                        fout[f"{base}light_{suffix}"] = h[:, i_bin, category, 1]    #1 = uds
+                        fout[f"{base}light_{suffix}"] = h[:, i_bin, category, 1]  # 1 = uds
                     else:
                         fout[f"{base}_{suffix}"] = h[:, i_bin, category, sum]
 
@@ -162,7 +162,7 @@ def main(args):
         setup = json.load(f)
     with Path("pmap_run3.json").open() as f:
         pmap = json.load(f)
-        
+
     do_BDT_regions = setup.get("do_BDT_regions", False)
 
     for region_key, reg_cfg in setup["categories"].items():
@@ -178,15 +178,13 @@ def main(args):
 
         obs = setup["observable"]
         region_to_load = REGION_MAP.get(region_key, region_key)
-        if do_BDT_regions and not "control" in region_to_load:
+        if do_BDT_regions and "control" not in region_to_load:
             region_to_load = region_to_load + "-BDT"
 
         # Determine Data Stream (e.g., EGammadata for zgamma)
         if "zgamma" in region_to_load:
             data_map_key = "EGammadata"
-        elif "zmumu" in region_to_load:
-            data_map_key = "Muondata"
-        elif "tt" in region_to_load:
+        elif "zmumu" in region_to_load or "tt" in region_to_load:
             data_map_key = "Muondata"
         else:
             data_map_key = "Jetdata"
@@ -265,7 +263,8 @@ def main(args):
                 for dataset in datasets:
                     events = utils.load_samples(
                         data_dir=Path(
-                            f"{args.skim_base}/{args.tag}/{args.year}"
+                            # f"/eos/uscms/store/group/lpchbbrun3/skims/{args.tag}/{args.year}"
+                            f"/eos/uscms/store/group/lpchbbrun3/gmachado/{args.tag}/{args.year}"
                         ),
                         samples={process: [dataset]},
                         columns=cols,
@@ -317,7 +316,9 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Unified Histogram Maker for Signal and CR")
-    parser.add_argument("--year", required=True, choices=["2022", "2022EE", "2023", "2023BPix", "2024"])
+    parser.add_argument(
+        "--year", required=True, choices=["2022", "2022EE", "2023", "2023BPix", "2024"]
+    )
     parser.add_argument("--tag", required=True, help="Tag for the skims directory (e.g., 26Feb03)")
     parser.add_argument(
         "--skim-base",
